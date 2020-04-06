ecd0928935396e94032c4f3358f75e5e4420d05d
Nigel Agwah <nigel.agwah@gmail.com>
2020-04-06 11:19:17 -0500

    Updated

--- alpha/api.py
+++ alpha/api.py
@@ -1,10 +1,14 @@
 import pandas as pd
 from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.foreignexchange import ForeignExchange
+import matplotlib.pyplot as plt
 
 keys='8C0B4ERLNTL4KOMW'
 
 df = ForeignExchange (key=keys, output_format='pandas')
-ins =df.get_currency_exchange_daily(from_symbol='USD', to_symbol='CAD',outputsize='compact')
+data, meta_data =df.get_currency_exchange_daily(from_symbol='USD', to_symbol='CAD',outputsize='compact')
 
-print(ins)
+data['close'].plot()
+plt.title('My First Observation')
+plt.show()
+
