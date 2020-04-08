46b08db8f0901cec96a3c92c0d1968c059ac2ae5
Nigel Agwah <nigel.agwah@gmail.com>
2020-04-08 00:17:44 -0500

    Add files via upload

--- todo/math.py
+++ todo/math.py
@@ -1,14 +1,20 @@
+import os
 import time
-n = 2
-m = -n 
-
+home = os.path.expanduser('~')
+os.chdir(home)
+cwd = os.getcwd()
+print(cwd)
+n = 50
+m = -n
+files = open('samples','a')
 while n > m :
  print ('N is ' +str(n) )
+ files.write('\n''N is '+str(n)+' M is '+ str(m))
  time.sleep(1)
  n-=1
  m+=1
-  if n == m:
-    print ('N at '+str(n)+' M at '+str(m) )
-    break
-    print ('curse is broken')
-
+ if n == m:
+  print ('N at '+str(n)+' M at '+str(m) )
+  break
+  print ('curse is broken')
+  files.write('\n curse is broken')
