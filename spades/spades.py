1ebe461946f59acbb6f7b5b6a092f4dc31a126cf
K547NK <nigel.agwah@gmail.com>
2021-04-07 19:05:18 -0500

    create game

---
+++ spades/spades.py
@@ -0,0 +1,30 @@
+''''spades games
+    bid  x10
+    bag  x1 10B = -100p 
+    NIL  100
+    BLIND NIL 200
+    TOTAL 13
+    SPADES RULES
+    A,K,Q,J,10,9,8,7,6,5,4,3,2
+    DECLARATION 
+    HEARTS   = H
+    DIAMONDS = D
+    SPADES   = S
+    CLUBS    = C
+
+'''
+
+# Declare Card Values
+2C, 2D, 2H = 1
+3C, 3D, 3H = 2
+4C, 4D, 4H = 3
+5C, 5D, 5H = 4
+6C, 6D, 6H = 5
+7C, 7D, 7H = 6
+8C, 8D, 8H = 7
+9C, 9D, 9H = 8
+10C, 10D, 10H = 9
+JC, JD, JH = 10
+QC, KD, KH = 11
+KC, KD, KH = 12
+AC, AD, AH = 13
